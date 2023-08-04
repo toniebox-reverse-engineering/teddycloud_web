f9ea72588a8bc328dbed47830b04fb875ae0acec
Claudio Götz <goetz@netz.werk.ch>
2023-08-04 02:31:19 +0200

    relad stats after 10s but load it once component is mounted

--- src/pages/home/StatsPage.tsx
+++ src/pages/home/StatsPage.tsx
@@ -18,14 +18,17 @@ export const StatsPage = () => {
   const { t } = useTranslation();
   const [stats, setStats] = useState<StatsList | undefined>();
 
-  useEffect(() => {
-    const fetchStats = async () => {
-      const statsRequest = (await api.apiStatsGet()) as StatsList;
-      if (statsRequest?.stats?.length && statsRequest?.stats?.length > 0) {
-        setStats(statsRequest);
-      }
-    };
+  const fetchStats = async () => {
+    const statsRequest = (await api.apiStatsGet()) as StatsList;
+    if (statsRequest?.stats?.length && statsRequest?.stats?.length > 0) {
+      setStats(statsRequest);
+    }
+  };
 
+  useEffect(() => {
+    fetchStats();
+  }, []);
+  useEffect(() => {
     const interval = setInterval(() => {
       fetchStats();
     }, 1000 * 10);
