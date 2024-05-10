dd5b97702fde9010b0cb40b7313a18e54af6eb2c
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-05-10 16:05:41 +0200

    code formatting

--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -2,7 +2,7 @@ import { Breadcrumb, Layout, Menu } from "antd";
 import Sider from "antd/es/layout/Sider";
 import Item from "antd/es/list/Item";
 import styled from "styled-components";
-import { theme } from 'antd';
+import { theme } from "antd";
 
 const { useToken } = theme;
 const useThemeToken = () => useToken().token;
