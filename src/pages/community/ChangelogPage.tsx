4b68dd1233e97f65452e2fe675c3bf3b8848af17
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-05-16 21:55:17 +0200

    updated changelog

--- src/pages/community/ChangelogPage.tsx
+++ src/pages/community/ChangelogPage.tsx
@@ -22,10 +22,12 @@ export const ChangelogPage = () => {
             version: "0.5.3",
             changes: [
                 "Stabilization backend",
+                "store last played time of last played tag",
                 "new /web gui: enhanced Toniecard, added search for Radiostream in Edit Modal",
                 "new /web gui: Tonies - Content section - added Migrate Content to Lib button on TAF files",
                 "new /web gui: added RTNL Log Page",
                 "new /web gui: added Json File Modal in Content section (double click on json file row)",
+                "new /web gui: show date time of last played tag (toniecard + toniboxcard)",
             ],
             commits: [
                 "https://github.com/toniebox-reverse-engineering/teddycloud/compare/tc_v0.5.2...tc_v0.5.3",
