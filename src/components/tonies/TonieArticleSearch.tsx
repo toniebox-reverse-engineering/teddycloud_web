ffbe0c46b62313523ca41007c3fda307f29b42b4
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-05-22 00:13:30 +0200

    separated components

--- src/components/tonies/TonieArticleSearch.tsx
+++ src/components/tonies/TonieArticleSearch.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Col, Form, Input, Modal, Row, Select, Tooltip, message } from "antd";
+import { Button, Select, Tooltip, message } from "antd";
 import type { SelectProps } from "antd";
 import { TonieInfo } from "./TonieCard";
+import ToniesCustomJsonEditor from "./ToniesCustomJsonEditor";
 
 export const TonieArticleSearch: React.FC<{
     placeholder: string;
@@ -17,15 +18,6 @@ export const TonieArticleSearch: React.FC<{
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [tonieInfos, setTonieInfos] = useState<TonieInfo[]>();
     const [showAddCustomTonieModal, setShowAddCustomTonieModal] = useState<boolean>(false);
-    const [form] = Form.useForm();
-
-    // Initialize the form with at least one empty field for each Form.List
-    useEffect(() => {
-        form.setFieldsValue({
-            ids: [{ "audio-id": '', "hash": '' }],
-            tracks: [{ "track": '' }],
-        });
-    }, [form]);
 
     const handleSearch = async (search: string) => {
         const searchEncode = encodeURIComponent(search);
@@ -65,182 +57,6 @@ export const TonieArticleSearch: React.FC<{
         setShowAddCustomTonieModal(true);
     };
 
-    const handleAddCustomTonieModalCancel = () => {
-        resetForm();
-        setShowAddCustomTonieModal(false);
-    }
-
-    const handleFinish = async (values: any) => {
-
-
-        if (values["track-desc"]) {
-            values["track-desc"] = values["track-desc"]
-                .filter((track: { track: string }) => track.track && track.track.trim())
-                .map((track: { track: string }) => track.track);
-        }
-        if (values.ids) {
-            values.ids = values.ids.filter((ids: { "audio-id": string, hash: string }) => ids["audio-id"] && ids["audio-id"].trim() && ids.hash && ids.hash.trim());
-        }
-        const url = `${process.env.REACT_APP_TEDDYCLOUD_API_URL}/api/doSomething/`;
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(values),
-            });
-            if (!response.ok) {
-                throw new Error(response.status + " " + response.statusText);
-            }
-
-            if (values.article) {
-                // call tonies api reload
-                // then set article value
-                setValue(values.article);
-                props.onChange(values.article);
-            }
-
-            resetForm();
-            message.success(t("tonies.addNewCustomTonieModal.successfullyCreated"));
-            setShowAddCustomTonieModal(false);
-
-        } catch (error) {
-            message.error(t("tonies.addNewCustomTonieModal.failedToCreate") + error);
-        }
-    };
-
-    const resetForm = () => {
-        form.resetFields();
-        form.setFieldsValue({
-            ids: [{ "audio-id": '', "hash": '' }],
-            tracks: [{ "track": '' }],
-        });
-    }
-
-    const handleOk = () => {
-        form.submit();
-    };
-
-    const addCustomTonieModal = (
-        <Modal title={t("tonies.addNewCustomTonieModal.title")} open={showAddCustomTonieModal} onCancel={handleAddCustomTonieModalCancel} onOk={handleOk} okText={t("tonies.addNewCustomTonieModal.save")} width={Math.max(window.innerWidth * 0.75, 500)}>
-            <Form form={form} layout="vertical" onFinish={handleFinish}>
-                <Row gutter={[16, 0]}>
-                    <Col span={24}>
-                        <Row gutter={[16, 0]}>
-                            <Col span={10}>
-                                <Form.Item label={t("tonies.addNewCustomTonieModal.series")} name="series" required>
-                                    <Input style={{ width: '100%' }} />
-                                </Form.Item>
-                            </Col>
-                            <Col span={10}>
-                                <Form.Item label={t("tonies.addNewCustomTonieModal.episode")} name="episode">
-                                    <Input style={{ width: '100%' }} />
-                                </Form.Item>
-                            </Col>
-                            <Col span={4}>
-                                <Form.Item label={t("tonies.addNewCustomTonieModal.language")} name="language">
-                                    <Input style={{ width: '100%' }} />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col span={24}>
-                        <Row gutter={[16, 0]}>
-                            <Col span={6}>
-                                <Form.Item label={t("tonies.addNewCustomTonieModal.model")} name="article">
-                                    <Input style={{ width: '100%' }} />
-                                </Form.Item>
-                            </Col>
-                            <Col span={18}>
-                                <Form.Item label={t("tonies.addNewCustomTonieModal.pic")} name="image">
-                                    <Input style={{ width: '100%' }} />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col span={24}>
-                        <Row gutter={[16, 0]}>
-                            <Col span={24}>
-                                <Form.List name="ids">
-                                    {(fields, { add, remove }) => (
-                                        <>
-                                            {fields.map(({ key, name, ...restField }, index) => (
-                                                <Row key={key} gutter={[16, 0]}>
-                                                    <Col span={6}>
-                                                        <Form.Item
-                                                            {...restField}
-                                                            name={[name, 'audio-id']}
-                                                            label={index === 0 ? t("tonies.addNewCustomTonieModal.audioId") : ''}
-                                                        >
-                                                            <Input style={{ width: '100%' }} />
-                                                        </Form.Item>
-                                                    </Col>
-                                                    <Col span={18}>
-                                                        <Form.Item
-                                                            {...restField}
-                                                            name={[name, 'hash']}
-                                                            label={index === 0 ? t("tonies.addNewCustomTonieModal.hash") : ''}
-                                                        >
-                                                            <Input style={{ width: '100%' }} addonAfter={<Button
-                                                                type="link"
-                                                                onClick={() => remove(name)}
-                                                                style={{ height: "auto", margin: -2 }}
-                                                            >
-                                                                {t("tonies.addNewCustomTonieModal.remove")}
-                                                            </Button>} />
-                                                        </Form.Item>
-                                                    </Col>
-                                                </Row>
-                                            ))}
-                                            <Form.Item>
-                                                <Button type="dashed" onClick={() => add()} block>
-                                                    {t("tonies.addNewCustomTonieModal.addAudioIdHash")}
-                                                </Button>
-                                            </Form.Item>
-                                        </>
-                                    )}
-                                </Form.List>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col span={24}>
-                        <Form.List name="track-desc">
-                            {(fields, { add, remove }) => (
-                                <>
-                                    {fields.map(({ key, name, ...restField }, index) => (
-                                        <Row key={key} gutter={[16, 0]}>
-                                            <Col span={24}>
-                                                <Form.Item
-                                                    {...restField}
-                                                    name={[name, 'track']}
-                                                    label={index === 0 ? t("tonies.addNewCustomTonieModal.track") : ''}
-                                                >
-                                                    <Input style={{ width: '100%' }} addonAfter={<Button
-                                                        type="link"
-                                                        onClick={() => remove(name)}
-                                                        style={{ height: "auto", margin: -2 }}
-                                                    >
-                                                        {t("tonies.addNewCustomTonieModal.remove")}
-                                                    </Button>} />
-                                                </Form.Item>
-                                            </Col>
-
-                                        </Row>
-                                    ))}
-                                    <Form.Item>
-                                        <Button type="dashed" onClick={() => add()} block>
-                                            {t("tonies.addNewCustomTonieModal.addTrack")}
-                                        </Button>
-                                    </Form.Item>
-                                </>
-                            )}
-                        </Form.List>
-                    </Col>
-                </Row>
-            </Form>
-        </Modal>);
-
     return (
         <>
             <Select
@@ -258,7 +74,12 @@ export const TonieArticleSearch: React.FC<{
                     label: d.text,
                 }))}
             />
-            {addCustomTonieModal}
+            <ToniesCustomJsonEditor
+                visible={showAddCustomTonieModal}
+                props={props}
+                setValue={setValue}
+                onClose={() => setShowAddCustomTonieModal(false)}
+            />
             <Tooltip title={t("tonies.addNewCustomTonieHint")}>
                 <Button
                     onClick={handleAddNewCustomButtonClick}
