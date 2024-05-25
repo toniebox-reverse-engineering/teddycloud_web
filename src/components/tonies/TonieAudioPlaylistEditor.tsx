1b8ba95f95793fd5b9356c79c37ed969956ca07e
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-05-25 22:45:02 +0200

    show json after save to allow user to copy it to the file manually

--- src/components/tonies/TonieAudioPlaylistEditor.tsx
+++ src/components/tonies/TonieAudioPlaylistEditor.tsx
@@ -3,6 +3,8 @@ import { Modal, Form, Input, Button, Space } from "antd";
 import { CloseOutlined, FolderOpenOutlined, MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 import { FileBrowser } from "./FileBrowser";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark, oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export interface FileItem {
     filepath: string;
@@ -116,134 +118,194 @@ const TonieAudioPlaylistEditor: React.FC<TonieAudioPlaylistEditorProps> = ({
         setSelectedFiles([]);
     };
 
-    return (
+    // just for now, can be removed later when API is available
+    const [jsonData, setJsonData] = useState<string>("");
+    const [jsonViewerModalOpened, setJsonViewerModalOpened] = useState(false);
+
+    const jsonViewerModalFooter = (
+        <Button type="primary" onClick={() => setJsonViewerModalOpened(false)}>
+            {t("tonies.informationModal.ok")}
+        </Button>
+    );
+
+    const handleJsonViewerModalClose = () => {
+        setJsonViewerModalOpened(false);
+    };
+
+    function detectColorScheme() {
+        const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        const storedTheme = localStorage.getItem("theme");
+
+        if (storedTheme === "auto") {
+            return prefersDarkMode ? "dark" : "light";
+        } else {
+            return storedTheme;
+        }
+    }
+
+    const jsonViewerModal = (
         <Modal
-            open={open}
-            title={initialValuesJson ? t("tonies.tapEditor.titleEdit") : t("tonies.tapEditor.titleCreate")}
-            okText={initialValuesJson ? t("tonies.tapEditor.save") : t("tonies.tapEditor.create")}
-            cancelText={t("tonies.tapEditor.cancel")}
-            onCancel={() => {
-                onCancel();
-                resetForm();
-            }}
-            onOk={() => {
-                form.validateFields()
-                    .then(() => {
-                        onCreate(form.getFieldsValue() as FormValues);
-                        resetForm();
-                    })
-                    .catch((info) => {
-                        console.log("Validate Failed:", info);
-                    });
-            }}
+            footer={jsonViewerModalFooter}
+            width={700}
+            title={"File (you can copy the content to a *.tap file)"}
+            open={jsonViewerModalOpened}
+            onCancel={handleJsonViewerModalClose}
         >
-            <Form
-                form={form}
-                layout="vertical"
-                name="tapEditorModal"
-                initialValues={{
-                    type: "tap",
-                    filepath: "",
-                    name: "",
-                    files: [],
+            {jsonData ? (
+                <SyntaxHighlighter
+                    language="json"
+                    style={detectColorScheme() === "dark" ? oneDark : oneLight}
+                    customStyle={{
+                        padding: 0,
+                        borderRadius: 0,
+                        margin: 0,
+                        border: "none",
+                    }}
+                >
+                    {jsonData}
+                </SyntaxHighlighter>
+            ) : (
+                "Loading..."
+            )}
+        </Modal>
+    );
+    // end removal json viewer
+
+    return (
+        <>
+            <Modal
+                open={open}
+                title={initialValuesJson ? t("tonies.tapEditor.titleEdit") : t("tonies.tapEditor.titleCreate")}
+                okText={initialValuesJson ? t("tonies.tapEditor.save") : t("tonies.tapEditor.create")}
+                cancelText={t("tonies.tapEditor.cancel")}
+                onCancel={() => {
+                    onCancel();
+                    resetForm();
+                }}
+                onOk={() => {
+                    form.validateFields()
+                        .then(() => {
+                            onCreate(form.getFieldsValue() as FormValues);
+                            //remove that if the API is available
+                            setJsonData(JSON.stringify(form.getFieldsValue(), null, 2));
+                            setJsonViewerModalOpened(true);
+                            resetForm();
+                        })
+                        .catch((info) => {
+                            console.log("Validate Failed:", info);
+                        });
                 }}
             >
-                <Form.Item name="audio_id" label="Audio ID">
-                    <Input type="number" />
-                </Form.Item>
-                <Form.Item name="filepath" label={t("tonies.tapEditor.filePath")}>
-                    <Input onChange={handleSourceInputChange} />
-                </Form.Item>
-                <Form.Item
-                    name="name"
-                    label={t("tonies.tapEditor.name")}
-                    rules={[{ required: true, message: t("tonies.tapEditor.nameRequired") }]}
+                <Form
+                    form={form}
+                    layout="vertical"
+                    name="tapEditorModal"
+                    initialValues={{
+                        type: "tap",
+                        filepath: "",
+                        name: "",
+                        files: [],
+                    }}
                 >
-                    <Input />
-                </Form.Item>
+                    <Form.Item name="audio_id" label="Audio ID">
+                        <Input type="number" />
+                    </Form.Item>
+                    <Form.Item name="filepath" label={t("tonies.tapEditor.filePath")}>
+                        <Input onChange={handleSourceInputChange} />
+                    </Form.Item>
+                    <Form.Item
+                        name="name"
+                        label={t("tonies.tapEditor.name")}
+                        rules={[{ required: true, message: t("tonies.tapEditor.nameRequired") }]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-                <Form.List name="files">
-                    {(fields, { add, remove }) => (
-                        <>
-                            {fields.map(({ key, name }, index) => (
-                                <div className="playlistTitle">
-                                    <Space
-                                        key={key}
-                                        style={{
-                                            display: "flex",
-                                            marginBottom: 8,
-                                            alignItems: "center",
-                                            width: "100%",
-                                        }}
-                                        align="baseline"
+                    <Form.List name="files">
+                        {(fields, { add, remove }) => (
+                            <>
+                                {fields.map(({ key, name }, index) => (
+                                    <div className="playlistTitle">
+                                        <Space
+                                            key={key}
+                                            style={{
+                                                display: "flex",
+                                                marginBottom: 8,
+                                                alignItems: "center",
+                                                width: "100%",
+                                            }}
+                                            align="baseline"
+                                        >
+                                            <Form.Item
+                                                name={[name, "filepath"]}
+                                                label={t("tonies.tapEditor.filePathContentFile")}
+                                                rules={[
+                                                    {
+                                                        required: true,
+                                                        message: t("tonies.tapEditor.filePathContentFileRequired"),
+                                                    },
+                                                ]}
+                                            >
+                                                <Input
+                                                    width="auto"
+                                                    addonBefore={
+                                                        <CloseOutlined
+                                                            onClick={() => {
+                                                                const newValues = [...form.getFieldsValue().files];
+                                                                newValues[index].filepath = "";
+                                                                form.setFieldsValue({ files: newValues });
+                                                            }}
+                                                        />
+                                                    }
+                                                    addonAfter={
+                                                        <FolderOpenOutlined onClick={() => handleEditFile(index)} />
+                                                    }
+                                                />
+                                            </Form.Item>
+                                            <Form.Item
+                                                name={[name, "name"]}
+                                                label={t("tonies.tapEditor.fileNameContentFile")}
+                                            >
+                                                <Input placeholder="Name" />
+                                            </Form.Item>
+                                            <MinusCircleOutlined onClick={() => remove(name)} />
+                                        </Space>
+                                    </div>
+                                ))}
+                                <Form.Item>
+                                    <Button
+                                        type="dashed"
+                                        onClick={() => showFileSelectModal()}
+                                        block
+                                        icon={<PlusOutlined />}
                                     >
-                                        <Form.Item
-                                            name={[name, "filepath"]}
-                                            label={t("tonies.tapEditor.filePathContentFile")}
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: t("tonies.tapEditor.filePathContentFileRequired"),
-                                                },
-                                            ]}
-                                        >
-                                            <Input
-                                                width="auto"
-                                                addonBefore={
-                                                    <CloseOutlined
-                                                        onClick={() => {
-                                                            const newValues = [...form.getFieldsValue().files];
-                                                            newValues[index].filepath = "";
-                                                            form.setFieldsValue({ files: newValues });
-                                                        }}
-                                                    />
-                                                }
-                                                addonAfter={
-                                                    <FolderOpenOutlined onClick={() => handleEditFile(index)} />
-                                                }
-                                            />
-                                        </Form.Item>
-                                        <Form.Item
-                                            name={[name, "name"]}
-                                            label={t("tonies.tapEditor.fileNameContentFile")}
-                                        >
-                                            <Input placeholder="Name" />
-                                        </Form.Item>
-                                        <MinusCircleOutlined onClick={() => remove(name)} />
-                                    </Space>
-                                </div>
-                            ))}
-                            <Form.Item>
-                                <Button
-                                    type="dashed"
-                                    onClick={() => showFileSelectModal()}
-                                    block
-                                    icon={<PlusOutlined />}
-                                >
-                                    {t("tonies.tapEditor.addFile")}
-                                </Button>
-                            </Form.Item>
-                        </>
-                    )}
-                </Form.List>
-            </Form>
-            <Modal
-                title={t("tonies.selectFileModal.selectFile")}
-                open={isSelectFileModalOpen}
-                onOk={handleOkSelectFile}
-                onCancel={handleCancelSelectFile}
-                width="auto"
-            >
-                <FileBrowser
-                    maxSelectedRows={99}
-                    special="library"
-                    trackUrl={false}
-                    selectTafOnly={false}
-                    key={filebrowserKey}
-                    onFileSelectChange={handleFileSelectChange}
-                />
+                                        {t("tonies.tapEditor.addFile")}
+                                    </Button>
+                                </Form.Item>
+                            </>
+                        )}
+                    </Form.List>
+                </Form>
+                <Modal
+                    title={t("tonies.selectFileModal.selectFile")}
+                    open={isSelectFileModalOpen}
+                    onOk={handleOkSelectFile}
+                    onCancel={handleCancelSelectFile}
+                    width="auto"
+                >
+                    <FileBrowser
+                        maxSelectedRows={99}
+                        special="library"
+                        trackUrl={false}
+                        selectTafOnly={false}
+                        key={filebrowserKey}
+                        onFileSelectChange={handleFileSelectChange}
+                    />
+                </Modal>
             </Modal>
-        </Modal>
+            {/* remove next line later when API is available */}
+            {jsonViewerModal}
+        </>
     );
 };
 
