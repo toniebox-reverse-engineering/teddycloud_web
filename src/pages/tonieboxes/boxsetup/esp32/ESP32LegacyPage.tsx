8834e74c726eb546d60ee459ab04420afbe6ffd1
henryk86 <166761451+henryk86@users.noreply.github.com>
2024-10-21 20:19:43 +0200

    esp32 legacy, skip dns step if already done in previous step

--- src/pages/tonieboxes/boxsetup/esp32/ESP32LegacyPage.tsx
+++ src/pages/tonieboxes/boxsetup/esp32/ESP32LegacyPage.tsx
@@ -144,6 +144,7 @@ esptool.py -b 921600 write_flash 0x0 tb.esp32.fakeca.bin`}
     const contentStep2 = (
         <>
             <h3>{t("tonieboxes.boxFlashingCommon.dns")}</h3>
+            <Paragraph>{t("tonieboxes.esp32BoxFlashing.legacy.skipDnsIfAlreadyDone")}</Paragraph>
             {dnsForTeddyCloud()}
         </>
     );
@@ -281,15 +282,13 @@ esptool.py -b 921600 write_flash 0x0 tb.esp32.fakeca.bin`}
                                 >
                                     {previousButton}
                                     <div>
-                                        <Button icon={<EyeOutlined />} onClick={checkBoxes}>
+                                        <Button icon={<EyeOutlined />} type="primary" onClick={checkBoxes}>
                                             {t("tonieboxes.esp32BoxFlashing.legacy.checkBoxes")}
                                         </Button>
                                     </div>
                                     <div>
-                                        <Button icon={<CheckSquareOutlined />} type="primary" onClick={next}>
-                                            {t(
-                                                "tonieboxes.esp32BoxFlashing.legacy.certificatesDumpedCAreplacementFlashed"
-                                            )}
+                                        <Button icon={<RightOutlined />} onClick={next}>
+                                            {t("tonieboxes.esp32BoxFlashing.legacy.proceedWithDNS")}
                                         </Button>
                                     </div>
                                 </div>
