5f2558b98fdc4050fdff0b06923c4d65c690279e
henryk86 <166761451+henryk86@users.noreply.github.com>
2025-03-25 20:34:31 +0100

    try to fix broken radiobrowser url and get available urls from all servers list

--- src/components/utils/RadioStreamSearch.tsx
+++ src/components/utils/RadioStreamSearch.tsx
@@ -16,35 +16,84 @@ export const RadioStreamSearch: React.FC<{
     const [data, setData] = useState<SelectProps["options"]>([]);
     const [value, setValue] = useState<string>();
 
+    const [radioBrowserAPIBaseJsonUrl, setRadioBrowserAPIBaseJsonUrl] = useState<string | null>(null);
     const [isRadioBrowserApiAvailable, setApiAvailable] = useState(false);
 
     // we use https://www.radio-browser.info/ as source for available radio stations
-    const radioBrowserAPIBaseJsonUrl = "https://de1.api.radio-browser.info/json/";
+    const fetchRadioBrowserBaseUrls = async () => {
+        try {
+            const response = await fetch("http://all.api.radio-browser.info/json/servers");
+            if (!response.ok) throw new Error("Failed to fetch server list");
+            const data = await response.json();
+            return data.map((server: { name: any }) => `https://${server.name}`);
+        } catch (error) {
+            console.error("Error fetching server list:");
+            return [];
+        }
+    };
+
+    const getRandomRadioBrowserUrl = async () => {
+        const hosts = await fetchRadioBrowserBaseUrls();
+        if (hosts.length === 0) return null;
+        return hosts[Math.floor(Math.random() * hosts.length)];
+    };
+
     useEffect(() => {
+        const fetchData = async () => {
+            const url = await getRandomRadioBrowserUrl();
+            if (!url) {
+                console.log("Fetching random radio browser URL failed, using hardcoded fallback.");
+                setRadioBrowserAPIBaseJsonUrl("https://de2.api.radio-browser.info/json");
+            } else {
+                console.log("Using random Radio Browser API URL:", url);
+                setRadioBrowserAPIBaseJsonUrl(url + "/json");
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (!radioBrowserAPIBaseJsonUrl) return;
+
         const checkApiAvailability = async () => {
             try {
-                const response = await fetch(radioBrowserAPIBaseJsonUrl + "stats");
+                console.log("Checking API availability at:", radioBrowserAPIBaseJsonUrl);
+                const response = await fetch(radioBrowserAPIBaseJsonUrl + "/stats");
+
                 if (response.ok) {
+                    console.log("API is available:", radioBrowserAPIBaseJsonUrl);
                     setApiAvailable(true);
+                } else {
+                    console.warn("API not available:", radioBrowserAPIBaseJsonUrl);
+                    setApiAvailable(false);
                 }
             } catch (error) {
-                console.log("Radio Browser API not available, disable radiostream search.");
+                console.error("Error checking API availability:", error);
                 setApiAvailable(false);
             }
         };
 
         checkApiAvailability();
-    }, []);
+    }, [radioBrowserAPIBaseJsonUrl]);
 
-    const debounce = (func: (...args: any[]) => void, wait: number) => {
-        let timeout: NodeJS.Timeout;
+    const useDebounce = (func: (...args: any[]) => void, wait: number) => {
+        const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+
         return (...args: any[]) => {
-            clearTimeout(timeout);
-            timeout = setTimeout(() => func.apply(this, args), wait);
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+            timeoutRef.current = setTimeout(() => func(...args), wait);
         };
     };
 
     const handleSearch = async (search: string) => {
+        if (!radioBrowserAPIBaseJsonUrl) {
+            console.warn("Skipping search: API URL is null");
+            return;
+        }
+
         setData([]);
         if (search === "") {
             return;
@@ -52,7 +101,7 @@ export const RadioStreamSearch: React.FC<{
         const searchEncode = encodeURIComponent(search);
 
         const url =
-            radioBrowserAPIBaseJsonUrl + "stations/search?name=" + searchEncode + "&is_https=true&hidebroken=true";
+            radioBrowserAPIBaseJsonUrl + "/stations/search?name=" + searchEncode + "&is_https=true&hidebroken=true";
         try {
             const response = await fetch(url, {});
             if (!response.ok) {
@@ -81,38 +130,38 @@ export const RadioStreamSearch: React.FC<{
         }
     };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const debouncedSearch = useCallback(debounce(handleSearch, 300), []);
+    const debouncedSearch = useCallback(useDebounce(handleSearch, 300), [radioBrowserAPIBaseJsonUrl]);
 
     const handleChange = (newValue: string) => {
         setValue(newValue);
         props.onChange(newValue);
     };
 
-    const radioStreamSearchInput = isRadioBrowserApiAvailable ? (
-        <>
-            <Typography.Text style={{ fontSize: "small", display: "inline-block", marginTop: "8px" }}>
-                {t("radioStreamSearch.searchLabel")}
-            </Typography.Text>
-            <Select
-                showSearch
-                style={{ margin: "8px 0" }}
-                value={value}
-                placeholder={props.placeholder}
-                defaultActiveFirstOption={false}
-                suffixIcon={null}
-                filterOption={false}
-                onSearch={debouncedSearch}
-                onChange={handleChange}
-                notFoundContent={null}
-                options={(data || []).map((d) => ({
-                    value: d.value,
-                    label: d.text,
-                }))}
-            />
-        </>
-    ) : (
-        <></>
-    );
+    const radioStreamSearchInput =
+        isRadioBrowserApiAvailable && radioBrowserAPIBaseJsonUrl ? (
+            <>
+                <Typography.Text style={{ fontSize: "small", display: "inline-block", marginTop: "8px" }}>
+                    {t("radioStreamSearch.searchLabel")}
+                </Typography.Text>
+                <Select
+                    showSearch
+                    style={{ margin: "8px 0" }}
+                    value={value}
+                    placeholder={props.placeholder}
+                    defaultActiveFirstOption={false}
+                    suffixIcon={null}
+                    filterOption={false}
+                    onSearch={debouncedSearch}
+                    onChange={handleChange}
+                    notFoundContent={null}
+                    options={(data || []).map((d) => ({
+                        value: d.value,
+                        label: d.text,
+                    }))}
+                />
+            </>
+        ) : (
+            <></>
+        );
     return radioStreamSearchInput;
 };
--- src/pages/community/ChangelogPage.tsx
+++ src/pages/community/ChangelogPage.tsx
@@ -16,7 +16,9 @@ export const ChangelogPage = () => {
             changes: [
                 "Stabilization backend",
                 "Fixed error activating RTNL when using TeddyCloud behind a reverse proxy https://github.com/toniebox-reverse-engineering/teddycloud/issues/291",
+                "Fixed error failed to read incomplete http response header https://github.com/toniebox-reverse-engineering/teddycloud/issues/326",
                 "gui: Added tonieboxes tab to homepage, user can now choose if he want to see his tonies or tonieboxes. Setting is stored locally.",
+                "gui: Changed Radio Browser API Url, try to get a working url from all servers list",
                 "gui: Some refactoring",
             ],
             commits: [
